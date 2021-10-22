36492957132bd4130f39d477ef26fd55bb4cbccb
David Hwang <davidhwang.ij@gmail.com>
2021-10-22 09:38:31 -0400

    Define functions

--- dns_forwarder.py
+++ dns_forwarder.py
@@ -18,20 +18,34 @@ def dns_record(dns_id):
         return 'AAAA'
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock.bind((UDP_IP, UDP_PORT))
+def udp_connect(ip, port):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((ip, port))
+    return sock
 
+
+def req_records(data):
+    qname = DNS(data)["DNS Question Record"].qname
+    qtype = dns_record(DNS(data)["DNS Question Record"].qtype)
+    return qname, qtype
+
+
+def ssl_connect(host):
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    wsock = context.wrap_socket(sock, server_hostname=host)
+    wsock.connect((host, 443))
+    return wsock
+
+
+sock = udp_connect(UDP_IP, UDP_PORT)
 data, addr = sock.recvfrom(512)
-qname = DNS(data)["DNS Question Record"].qname
-qtype = dns_record(DNS(data)["DNS Question Record"].qtype)
-print(f"qname: {qname}, qtype: {qtype}")
+domain, record_type = req_records(data)
+print(f"domain: {domain}, record type: {record_type}")
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-wsock = context.wrap_socket(sock, server_hostname=DOH_HOST)
-wsock.connect((DOH_HOST, 443))
+wsock = ssl_connect(DOH_HOST)
 
-request_msg = f"GET /dns-query?name={qname}&type={qtype} HTTP/1.1\r\nAccept: application/dns-json\r\nHost: 1.1.1.1\r\n\r\n"
+request_msg = f"GET /dns-query?name={domain}&type={record_type} HTTP/1.1\r\nAccept: application/dns-json\r\nHost: 1.1.1.1\r\n\r\n"
 wsock.send(request_msg.encode())
 
 data = wsock.recv(2048)
