4e3a0fb814589e229ed811f7bc7bca5d5580aea8
David Hwang <davidhwang.ij@gmail.com>
2021-10-23 16:54:59 -0400

    Check if in deny list and log

--- dns_forwarder.py
+++ dns_forwarder.py
@@ -1,10 +1,6 @@
 import socket
 import ssl
 from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
-import json
-import subprocess
-import sys
-import base64
 
 UDP_PORT = 53
 UDP_IP = "127.0.0.1"
@@ -27,9 +23,8 @@ def udp_connect(ip, port):
 def req_records(data):
     qname = DNS(data)["DNS Question Record"].qname
     qtype = dns_record(DNS(data)["DNS Question Record"].qtype)
-    src = IP(data)["IP"].src
-    sport = UDP(data)["UDP"].sport
-    return qname, qtype, src, sport
+    req_id = DNS(data)["DNS"].id
+    return qname, qtype, req_id
 
 
 def ssl_connect(host):
@@ -40,22 +35,63 @@ def ssl_connect(host):
     return wsock
 
 
+def write_to_log(hostname, record_type, isDenied):
+    print(f"hostname: {hostname}, record type: {record_type}")
+    status = "DENY" if isDenied else "ALLOW"
+    f = open("./queries.log", "a")
+    f.write(f"{hostname} {record_type} {status}\n")
+    f.close()
+
+
+def isin_deny_list(data, qname, record_type):
+    isDenied = False
+
+    decoded_qname = qname.decode("utf-8")
+    qname_length = len(decoded_qname)
+    hostname = decoded_qname[:qname_length-1]
+
+    with open('deny_list.txt') as f:
+        lines = f.readlines()
+
+    for line in lines:
+        # check if the flag exists
+        if (line == hostname + "\n"):
+            isDenied = True
+        write_to_log(hostname, record_type, isDenied)
+        if isDenied:
+            break
+
+    return isDenied
+
+
+def send_nxdomain(data, req_id):
+    # nx = IP(data)
+    # nx[DNS] = DNS()
+    print(f"data: {data}, req id: {req_id}")
+
+
 def main():
     sock = udp_connect(UDP_IP, UDP_PORT)
     req_data, addr = sock.recvfrom(512)
+    qname, record_type, req_id = req_records(req_data)
+    isDenied = isin_deny_list(req_data, qname, record_type)
+    if isDenied:
+        send_nxdomain(req_data, req_id)
 
-    content_length = len(req_data)
-    req_header = f"POST /dns-query HTTP/1.1\r\nContent-Type: application/dns-message\r\nContent-Length:{content_length}\r\nHost: 1.1.1.1\r\n\r\n"
-    req = bytes(req_header, 'utf-8') + req_data
+    print(f"Denied: {isDenied}")
 
-    wsock = ssl_connect(DOH_HOST)
+    # content_length = len(req_data)
+    # req_header = f"POST /dns-query HTTP/1.1\r\nContent-Type: application/dns-message\r\nContent-Length:{content_length}\r\nHost: 1.1.1.1\r\n\r\n"
+    # req = bytes(req_header, 'utf-8') + req_data
 
-    wsock.send(req)
-    data = wsock.recv(2048)
-    print(f"data: {data}")
-    data_body = data.split("\r\n\r\n".encode('utf-8'))[1]
-    print(f"body: {data_body}")
-    sock.sendto(data_body, addr)
+    # wsock = ssl_connect(DOH_HOST)
+
+    # wsock.send(req)
+    # data = wsock.recv(2048)
+    # print(f"data: {data}")
+    # data_body = data.split("\r\n\r\n".encode('utf-8'))[1]
+    # print(f"body: {data_body}")
+    # sock.sendto(data_body, addr)
 
 
 if __name__ == "__main__":
