ee6d872c3ab6e4effeb0df2c6b0cad75b20d1043
David Hwang <davidhwang.ij@gmail.com>
2021-10-24 00:30:05 -0400

    Added command line args and udp/doh forwarder

--- dns_forwarder.py
+++ dns_forwarder.py
@@ -1,10 +1,33 @@
 import socket
 import ssl
-from scapy.all import DNS, DNSQR, DNSRR, IP, send, sniff, sr1, UDP
+from scapy.all import DNS, DNSQR
+import argparse
+
+parser = argparse.ArgumentParser(
+    description="Run a DNS forwarder to forward your request to a DoH server")
+parser.add_argument('-d', type=str, metavar='', dest='DST_IP',
+                    help='Destination DNS server IP')
+parser.add_argument('-f', type=str, metavar='', default='deny_list.txt',
+                    help='DENY_LIST_FILE containing domains to block')
+parser.add_argument('-l', type=str, metavar='', default='./queries.log',
+                    help='LOG_FILE Append-only log file')
+parser.add_argument('--doh', type=bool, metavar='', dest='DOH', default=False,
+                    help='Use default upstream DoH server')
+parser.add_argument('--doh_server', type=str, metavar='', dest='DOH_SERVER',
+                    help='User this upstream DoH server')
+
+args = parser.parse_args()
 
 UDP_PORT = 53
-UDP_IP = "127.0.0.1"
-DOH_HOST = '1.1.1.1'
+UDP_IP = '127.0.0.1'
+DST_IP = args.DST_IP
+DOH_SERVER = args.DOH_SERVER if args.DOH_SERVER != None else '1.1.1.1'
+# if only DOH is specified???
+
+# Handle Flag Errors
+if (args.DOH == False and args.DOH_SERVER == None and args.DST_IP == None):
+    print("If --doh and --doh_server are not specified, you must specify your -d")
+    exit(0)
 
 
 def dns_record(dns_id):
@@ -77,33 +100,46 @@ def nxdomain(hostname, record_type, req_id):
     return bytes(nx)
 
 
+def udp_forward(req_data):
+    sock = udp_connect(UDP_IP, 12345)
+    sock.sendto(req_data, (DST_IP, UDP_PORT))
+    data, _ = sock.recvfrom(512)
+
+    return data
+
+
+def doh_forward(req_data):
+    content_length = len(req_data)
+    req_header = f"POST /dns-query HTTP/1.1\r\nContent-Type: application/dns-message\r\nContent-Length:{content_length}\r\nHost: 1.1.1.1\r\n\r\n"
+    req = bytes(req_header, 'utf-8') + req_data
+
+    wsock = ssl_connect(DOH_SERVER)
+    wsock.send(req)
+    data = wsock.recv(2048)
+    print(f"data: {data}")
+    data_body = data.split("\r\n\r\n".encode('utf-8'))[1]
+
+    return data_body
+
+
 def main():
     sock = udp_connect(UDP_IP, UDP_PORT)
-    req_data, addr = sock.recvfrom(512)
+    while True:
+        req_data, addr = sock.recvfrom(512)
+        hostname, record_type, req_id = req_records(req_data)
+        isDenied = isin_deny_list(hostname, record_type)
 
-    DNS(req_data).show()
+        if isDenied:
+            nx = nxdomain(hostname, record_type, req_id)
+            sock.sendto(nx, addr)
+        else:
+            if (args.DOH == False and args.DOH_SERVER == None):
+                # send over DNS
+                data = udp_forward(req_data)
+            else:
+                data = doh_forward(req_data)
 
-    hostname, record_type, req_id = req_records(req_data)
-    print(f"hostname: {hostname}")
-    isDenied = isin_deny_list(hostname, record_type)
-    if isDenied:
-        nx = nxdomain(hostname, record_type, req_id)
-        sock.sendto(nx, addr)
-
-    # print(f"Denied: {isDenied}")
-
-    # content_length = len(req_data)
-    # req_header = f"POST /dns-query HTTP/1.1\r\nContent-Type: application/dns-message\r\nContent-Length:{content_length}\r\nHost: 1.1.1.1\r\n\r\n"
-    # req = bytes(req_header, 'utf-8') + req_data
-
-    # wsock = ssl_connect(DOH_HOST)
-
-    # wsock.send(req)
-    # data = wsock.recv(2048)
-    # print(f"data: {data}")
-    # data_body = data.split("\r\n\r\n".encode('utf-8'))[1]
-    # print(f"body: {data_body}")
-    # sock.sendto(data_body, addr)
+            sock.sendto(data, addr)
 
 
 if __name__ == "__main__":
